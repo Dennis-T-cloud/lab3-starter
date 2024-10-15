eed4b1c8f57115fb8f41d4628a1d7ac80e6201bb
Sun <desun@ieng6-201.ucsd.edu>
2024-10-15 16:25:22 -0700

    Test commit dennis

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+//A bit change here! :) Dennis
+int main() {
+  char hello[] = "Hello! NEW Hello!"; 
+  char hello2[] = { 72, 101, 108, 108, 111, 33, 0 };
+
+  puts(hello);
+  puts(hello2);
+
+  return 0;
+}
